d00d86532e7b4fc2b7142f5423d32db183c7876c
Danish <95996366+Danish0123@users.noreply.github.com>
2022-06-04 11:15:32 +0530

    Add files via upload

---
+++ Ass3-2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+  int mian()
+  {
+  	int i,array[10],sum=0;
+  	for(i=0;i<10;i++)
+  	{
+  		printf("Enter numbers:");
+  		scanf("%d",&array[i]);
+  	}
+  	for(i=0;i<10;i++)
+  	{
+  		printf("%d",array[i]);	 
+	}
+	for(i=0;i<10;i++)
+	sum=sum+array[i];
+	printf("\nSum of array element=%d",sum); 
+	return 0;
+}
---
+++ Ass3-5.c
@@ -0,0 +1,15 @@
+  #include<stdio.h>
+  int mian()
+  {
+  	int i,array[10];
+  	for(i=0;i<10;i++)
+  	{
+  		printf("Enter numbers:");
+  		scanf("%d",&array[i]);
+  	}
+  	for(i=9;i>0;i--)
+  	{
+  		printf("%d",array[i]);	 
+	}
+	return 0;
+  }
